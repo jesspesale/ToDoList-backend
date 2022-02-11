4f292a4dd24a514dfa4e1a42e11d1610b607f12f
jesspesale <jess_pesale@aol.com>
2022-02-11 16:51:49 -0500

    added create action in todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,32 @@
 class TodosController < ApplicationController
 
-    before_action :set_todo only[:show, :update, :destroy]
+    before_action :set_todo, only: [:show, :update, :destroy]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 
 
-    
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            render json: @todo
+        else
+            render json: @todo.errors, status: :unprocessable_entity
+        end
+    end
 
     private
     # gets a todo based on its ID
